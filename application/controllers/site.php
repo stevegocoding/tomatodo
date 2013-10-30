357159c090e3af07ae6402897328980eae893959
stevegocoding <stevegocoding@gmail.com>
2013-10-30 14:43:35 -0700

    initial commit

---
+++ application/controllers/site.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: magkbdev
+ * Date: 10/30/13
+ * Time: 2:39 PM
+ */
+
+class Site extends CI_Controller {
+
+    public function index()
+    {
+        echo ("Hello!!!!!");
+    }
+} 
